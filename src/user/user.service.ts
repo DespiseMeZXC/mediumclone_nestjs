a406b5f95bc13e56c5bd9d4965ff1e0d5cdef7b0
Alexey <leyla.0705@mail.ru>
2023-05-02 22:24:29 +0300

    add types, jwttoken, and pretty answer

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,8 @@ import { createUserDto } from "./dto/createUser.dto";
 import { UserEntity } from "./user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import {sign} from "jsonwebtoken"
+import { UserResponseInterface } from "./types/userResponse.interface";
 
 @Injectable()
 export class UserService{
@@ -12,5 +14,21 @@ export class UserService{
         const newUser = new UserEntity();
         Object.assign(newUser, createUserDto);
         return await this.userReposetory.save(newUser);
+    }
+
+    generateJwt(user: UserEntity) : string{
+        return sign({
+            id: user.id,
+            username: user.username,
+            email: user.email,
+        }, process.env.KEY_TOKEN)
+    }
+    buildUserResponse(user: UserEntity) :UserResponseInterface{
+        return {
+            user : {
+            ...user,
+            token : this.generateJwt(user)
+        },
+        };
     }
 }
